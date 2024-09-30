5f25793e157807885e8344137bec27cb063775fe
utca216 <151619685+utca216@users.noreply.github.com>
2024-09-30 23:32:31 +0600

    1233

--- src/main/java/org/example/StringProcessing.java
+++ src/main/java/org/example/StringProcessing.java
@@ -4,10 +4,10 @@ public class StringProcessing {
     public boolean isStrongPassword(String password){
         boolean specialCharactersFound = false;
         String SpecialCharacters="!@#$%^&*()_+-=?><";
-        for(int i=0;i<password.size();i++){
+        for(int i=0;i<password.length();i++){
             if(specialCharactersFound)
                 break;
-            for(int j=0;j<SpecialCharacters.size();j++)
+            for(int j=0; j<SpecialCharacters.length(); j++)
                 if(password.charAt(i)==SpecialCharacters.charAt(j)) {
                     specialCharactersFound = true;
                     break;
@@ -24,10 +24,10 @@ public class StringProcessing {
 
         boolean NumbersFound = false;
         String NumbersCharacters ="0123456789";
-        for(int i=0;i<password.size();i++){
+        for(int i=0;i<password.length();i++){
             if(NumbersFound)
                 break;
-            for(int j = 0; j< NumbersCharacters.size(); j++)
+            for(int j = 0; j< NumbersCharacters.length(); j++)
                 if(password.charAt(i)== NumbersCharacters.charAt(j)) {
                     NumbersFound = true;
                     break;
@@ -39,9 +39,9 @@ public class StringProcessing {
 
     public int calculateDigits(String sentence){
         int digitsNumber=0;
-        String digits ="!@#$%^&*()_+-=?><";
-        for(int i=0;i<sentence.size();i++){
-            for(int j = 0; j< digits.size(); j++)
+        String digits ="0123456789";
+        for(int i=0;i<sentence.length();i++){
+            for(int j = 0; j< digits.length(); j++)
                 if(sentence.charAt(i)== digits.charAt(j))
                     digitsNumber++;
         }
@@ -50,14 +50,16 @@ public class StringProcessing {
 
     public int calculateWords(String sentence){
         int wordsNumber=0;
-        for(int i=0;i<sentence.size();i++){
+        if(sentence.charAt(0)!=32)
+            wordsNumber++;
+        for(int i=0;i<sentence.length();i++){
                 if(sentence.charAt(i)==32 && sentence.charAt(i+1)!=32)
                     wordsNumber++;
         }
         return wordsNumber;
     }
 
-    
+
 
 
 }
--- src/test/java/SringProsessingTest.java
+++ src/test/java/SringProsessingTest.java
@@ -1,2 +1,33 @@
+import org.example.StringProcessing;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class SringProsessingTest {
+    @Test void isStrongPasswordTest() {
+        StringProcessing sp = new StringProcessing();
+        assertTrue(sp.isStrongPassword("Abubus123*"));
+        assertTrue(sp.isStrongPassword("01kg674ach"));
+        assertTrue(sp.isStrongPassword("roba_doba_du!"));
+        assertTrue(sp.isStrongPassword("#gDdGq6|W"));
+        assertTrue(sp.isStrongPassword("QWERTY321!"));
+    }
+    @Test void calculateDigitsTest() {
+        StringProcessing sp = new StringProcessing();
+        assertEquals(3, sp.calculateDigits("Abubus123"));
+        assertEquals(1, sp.calculateDigits("Abubus1"));
+        assertEquals(10, sp.calculateDigits("Abubus1231231231"));
+        assertEquals(0, sp.calculateDigits("Abubus"));
+        assertEquals(3, sp.calculateDigits("A1bu2us3"));
+    }
+
+    @Test void calculateWordsTest() {
+        StringProcessing sp = new StringProcessing();
+        assertEquals(3, sp.calculateWords(" anoba aboba du"));
+        assertEquals(3, sp.calculateWords("anoba aboba du"));
+        assertEquals(1, sp.calculateWords("anobaabobadu"));
+        assertEquals(9, sp.calculateWords(" a n o b a a bo ba du"));
+        assertEquals(2, sp.calculateWords(" anoba ab"));
+    }
 }
