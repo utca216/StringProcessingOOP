7bc219c02585e3ea7d8ab4210c7d1cded2fa4c58
utca216 <151619685+utca216@users.noreply.github.com>
2024-09-30 22:41:45 +0600

    df

--- src/main/java/org/example/StringProcessing.java
+++ src/main/java/org/example/StringProcessing.java
@@ -2,8 +2,62 @@ package org.example;
 
 public class StringProcessing {
     public boolean isStrongPassword(String password){
-        boolean digit=(password.contains("0") || password.contains("1")|| password.contains("2")|| password.contains("3")|| password.contains("4")|| password.contains("5")|| password.contains("6")|| password.contains("7")|| password.contains("8")|| password.contains("9") )
-        boolean specialCaracter=(password.contains("!"))|| password.contains("@")|| password.contains("#")|| password.contains("$")|| password.contains("%")|| password.contains("^")|| password.contains("&")|| password.contains("*")|| password.contains("(")|| password.contains(")")|| password.contains("+")|| password.contains("_")|| password.contains("-"))
-        
+        boolean specialCharactersFound = false;
+        String SpecialCharacters="!@#$%^&*()_+-=?><";
+        for(int i=0;i<password.size();i++){
+            if(specialCharactersFound)
+                break;
+            for(int j=0;j<SpecialCharacters.size();j++)
+                if(password.charAt(i)==SpecialCharacters.charAt(j)) {
+                    specialCharactersFound = true;
+                    break;
+                }
+        }
+
+        boolean uppercaseFound = false;
+        if(password.toLowerCase()!=password)
+            uppercaseFound=true;
+
+        boolean lowerCaseFound = false;
+        if(password.toUpperCase()!=password)
+            lowerCaseFound=true;
+
+        boolean NumbersFound = false;
+        String NumbersCharacters ="0123456789";
+        for(int i=0;i<password.size();i++){
+            if(NumbersFound)
+                break;
+            for(int j = 0; j< NumbersCharacters.size(); j++)
+                if(password.charAt(i)== NumbersCharacters.charAt(j)) {
+                    NumbersFound = true;
+                    break;
+                }
+        }
+
+        return specialCharactersFound && uppercaseFound && lowerCaseFound && NumbersFound;
     }
+
+    public int calculateDigits(String sentence){
+        int digitsNumber=0;
+        String digits ="!@#$%^&*()_+-=?><";
+        for(int i=0;i<sentence.size();i++){
+            for(int j = 0; j< digits.size(); j++)
+                if(sentence.charAt(i)== digits.charAt(j))
+                    digitsNumber++;
+        }
+        return digitsNumber;
+    }
+
+    public int calculateWords(String sentence){
+        int wordsNumber=0;
+        for(int i=0;i<sentence.size();i++){
+                if(sentence.charAt(i)==32 && sentence.charAt(i+1)!=32)
+                    wordsNumber++;
+        }
+        return wordsNumber;
+    }
+
+    
+
+
 }
